415d6f6da67bf7cd07f56e52da3014705f928ec8
joseSoberon <jose.ant.soberon@gmail.com>
2022-09-22 21:57:24 -0500

    Create function to display main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("My Restaurant Page");
+import createMain from "./main";
+
+let main = document.querySelector("main");
+main.appendChild(createMain())
+console.log(main.firstElementChild.className);
+
+let nav = document.querySelectorAll("li");
+
+nav.forEach(item => {
+    item.addEventListener("click", () => {
+        if (item.id == "home" && main.firstElementChild.className != "inicio") {
+            console.log("hello");
+            main.removeChild(main.firstElementChild);
+            main.appendChild(createMain());
+        }
+        else if (item.id == "menu") {
+            main.removeChild(main.firstElementChild);
+            console.log(main.value
+        }
+    })
+})
---
+++ src/main.js
@@ -0,0 +1,46 @@
+import foodTruck from "./food-image.jpg";
+
+export default function createMain() {
+    let container = createDiv("inicio");
+    let descriptionDiv = createDiv("description");
+    let imageDiv = createDiv("image")
+
+    let title = "Los mejores tacos de tu esquina";
+    let heading = createHeading(title);
+    descriptionDiv.appendChild(heading);
+
+    let description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati id, laudantium fugit nobis ad explicabo quo sapiente est aut molestiae iusto ipsam placeat odit. Aliquid quaerat deserunt illum officiis sequi!";
+    let paragraph = createParagraph(description);
+    descriptionDiv.appendChild(paragraph);
+
+    let image = new Image();
+    image.src = foodTruck;
+    image.classList.add("image");
+    imageDiv.appendChild(image);
+
+    container.appendChild(descriptionDiv);
+    container.appendChild(imageDiv);
+
+    return container
+}
+
+function createDiv(className) {
+    let container = document.createElement("div");
+    container.classList.add(className)
+
+    return container;
+}
+
+function createHeading(text) {
+    let heading = document.createElement("h2");
+    heading.textContent = text;
+
+    return heading;
+}
+
+function createParagraph(text) {
+    let paragraph = document.createElement("p");
+    paragraph.textContent = text;
+
+    return paragraph;
+}
